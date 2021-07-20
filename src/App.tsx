9b8fed12c2f08774613fae7594c6e8295001b03d
Verex <zach.verex@gmail.com>
2021-07-20 12:21:38 -0400

    Working on webcam display.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.global.css';
-import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from 'react-player/youtube';
 
-const Hello = () => {
-  return (
-    <ReactPlayer 
-      url='https://www.youtube.com/watch?v=m_ZW1mxGh0s'
-      playing={true}
-      width='100%'
-      height='100%'
-    />
-  );
-};
+interface Props {}
+
+interface State {
+  videoStream: MediaStream | null;
+}
+class Application extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      videoStream: null,
+    };
+
+    this.getMedia();
+  }
+
+  getMedia = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+    this.setState({ videoStream: stream });
+  };
+
+  render() {
+    if (!this.state.videoStream) {
+      return <span>&#128512;</span>;
+    }
+
+    return (
+      <ReactPlayer
+        url={this.state.videoStream}
+        playing={true}
+        width="100%"
+        height="100%"
+      />
+    );
+  }
+}
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path="/" component={Application} />
       </Switch>
     </Router>
   );
